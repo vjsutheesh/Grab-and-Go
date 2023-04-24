4744c49e922cd9daecfedf1fdfa179034136275f
Kanish-SK <kanishshivan@gmail.com>
2023-04-24 08:26:08 +0530

    Connect authentication page with backend-end

--- front-end/src/components/Navbar/NavBar.js
+++ front-end/src/components/Navbar/NavBar.js
@@ -1,46 +1,57 @@
 import { Link } from "react-router-dom";
-import Grab from './Grab.png';
+import Grab from "./Grab.png";
 // import { useState,useEffect } from "react";
 import { MdWhereToVote, MdSearch } from "react-icons/md";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { VscAccount } from "react-icons/vsc";
 import { CgShoppingCart } from "react-icons/cg";
 const NavBar = () => {
-    return (
-        <div className="header home" >
-            <div className="image-logo">
-                <Link to='/' className="logo-link">
-                    <img src={Grab} alt="Loading...." />
-                </Link>
+  return (
+    <div className="header home">
+      <div className="image-logo">
+        <Link to="/" className="logo-link">
+          <img src={Grab} alt="Loading...." />
+        </Link>
+      </div>
+      <div className="header-right">
+        <div className="header-location-bar">
+          <div className="location-wrap">
+            <div className="location-icon">
+              <MdWhereToVote className="iconloc absolute-center" />
+              <div>Salem</div>
             </div>
-            <div className="header-right">
-                <div className="header-location-bar">
-                    <div className="location-wrap">
-                        <div className="location-icon">
-                            <MdWhereToVote className="iconloc absolute-center" />
-                            <div>Salem</div>
-                        </div>
-                        <IoMdArrowDropdown />
-                    </div>
-                    <div className="seperator"></div>
-                    <div className="search-bar">
-                        <MdSearch className="search-icon" />
-                        <input type="text" className="search-text" placeholder="Search for a restaurant" />
-                    </div>
-                </div>
-                <div className="profile-wrapper">
-                    <div className="cart-bar">
-                        <a href="/mycart" className="mycart-icon"><CgShoppingCart/></a>
-                        <a href="/mycart" className="mycart-name">My Cart</a>
-                    </div>
-                    <div className="account-details">
-                        <a href="/" className="header-username">Sutheesh</a>
-                        <a href="/"><VscAccount className="header-profile-image" /></a>
-                    </div>
-
-                </div>
-            </div>
+            <IoMdArrowDropdown />
+          </div>
+          <div className="seperator"></div>
+          <div className="search-bar">
+            <MdSearch className="search-icon" />
+            <input
+              type="text"
+              className="search-text"
+              placeholder="Search for a restaurant"
+            />
+          </div>
         </div>
-    );
-}
+        <div className="profile-wrapper">
+          <div className="cart-bar">
+            <a href="/mycart" className="mycart-icon">
+              <CgShoppingCart />
+            </a>
+            <a href="/mycart" className="mycart-name">
+              My Cart
+            </a>
+          </div>
+          <div className="account-details">
+            <a href="/" className="header-username">
+              {sessionStorage.getItem("username")}
+            </a>
+            <a href="/">
+              <VscAccount className="header-profile-image" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default NavBar;
--- front-end/src/components/loginpage/login.js
+++ front-end/src/components/loginpage/login.js
@@ -1,10 +1,60 @@
 import { Link, useHistory } from "react-router-dom";
 import Grab from "./Grab.png";
+import { useState, useEffect } from "react";
 
 const Login = () => {
   const history = useHistory();
-  const HandleSubmit = () => {
-    history.push("/home");
+
+  useEffect(() => {
+    sessionStorage.clear();
+  }, []);
+
+  const [inputName, setInputname] = useState("");
+  const [password, setPassword] = useState("");
+
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      fetch("http://127.0.0.1:5000/data")
+        .then((res) => {
+          return res.json();
+        })
+        .then((resp) => {
+          let flag = true;
+          {
+            Object.values(resp).map((userData) => {
+              if (userData["userName"] === inputName) {
+                if (userData["password"] === password) {
+                  flag = false;
+                  sessionStorage.setItem("username", inputName);
+                  history.push("/home");
+                }
+              }
+            });
+          }
+          if (flag) {
+            console.log("Invalid Username or Password");
+          } else {
+            console.log("Logged In successfully!");
+          }
+        })
+        .catch((err) => {
+          console.log("Login Failed due to :" + err.message);
+        });
+    }
+  };
+
+  const validate = () => {
+    let result = true;
+    if (inputName === null || inputName === "") {
+      result = false;
+      console.log("Please Enter Username");
+    }
+    if (password === null || password === "") {
+      result = false;
+      console.log("Please Enter Password");
+    }
+    return result;
   };
 
   return (
@@ -29,24 +79,26 @@ const Login = () => {
             <div className="logo">
               <img src={Grab} alt="Loading...." />
             </div>
-            <form className="login-form">
+            <form className="login-form" onSubmit={HandleSubmit}>
               <div className="input-icons">
                 <i className="fa fa-user icon"></i>
                 <input
                   type="text"
                   placeholder="Username"
                   className="username"
+                  value={inputName}
+                  onChange={(e) => setInputname(e.target.value)}
                 />
                 <i class="fa fa-key icon"></i>
                 <input
                   type="password"
                   placeholder="Password"
                   className="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <button className="submit-btn" onClick={HandleSubmit}>
-                Login
-              </button>
+              <button className="submit-btn">Login</button>
               <p className="account">
                 Don't have an account ?
                 <span>
