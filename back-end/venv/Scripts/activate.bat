e3ab805cadc77f5d4f5a4b1ff53c00ed06a3a6e6
SUTHEESH\vjsut <vjsutheesh@gmail.com>
2023-04-27 18:42:58 +0530

    back-end routes

--- back-end/venv/Scripts/activate.bat
+++ back-end/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ADMIN\Desktop\Grab-and-Go\back-end\venv
+set VIRTUAL_ENV=C:\Users\vjsut\Desktop\Mini Project\Grab-and-Go\back-end\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
